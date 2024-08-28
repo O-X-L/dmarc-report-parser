424a833439b9b16ffc108d018aa0d5a3d9a1eac8
Rath Pascal <rath@oxl.at>
2024-08-28 18:26:25 +0200

    init refactoring, replaced CI

---
+++ src/dmarc_report_parser/input/main.py
@@ -0,0 +1,9 @@
+from mailbox_connection import MailboxConnection
+from graph import MSGraphConnection
+from gmail import GmailConnection
+from imap import IMAPConnection
+
+__all__ = ["MailboxConnection",
+           "MSGraphConnection",
+           "GmailConnection",
+           "IMAPConnection"]
